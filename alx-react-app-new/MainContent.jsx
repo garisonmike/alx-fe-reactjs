d86399c42168a01aa7dcb506b39cb1c4d20a3960
megamindjr <dkimathi605@gmail.com>
2026-01-13 15:20:42 +0300

    Create MainContent.jsx

---
+++ alx-react-app-new/MainContent.jsx
@@ -0,0 +1,19 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: "20px",
+        backgroundColor: "#eaeaea",
+        minHeight: "250px"
+      }}
+    >
+      <h2 style={{ color: "#222" }}>Main Content</h2>
+      <p style={{ fontSize: "16px", lineHeight: "1.5" }}>
+        This section contains the main content of the application.
+        It highlights information about different cities and their uniqueness.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
