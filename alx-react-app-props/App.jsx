09f992df4813dbd90cfadf65e57161f069921eba
megamindjr <dkimathi605@gmail.com>
2026-01-18 21:05:14 +0300

    Create App.jsx

---
+++ alx-react-app-props/App.jsx
@@ -0,0 +1,15 @@
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    // Provide the userData value to the entire component tree
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
+}
+
+export default App;
