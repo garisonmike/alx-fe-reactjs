6d1932492271250e1804f6f023b0aef25e52210c
megamindjr <dkimathi605@gmail.com>
2026-01-25 15:18:15 +0300

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,12 +2,14 @@ import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
 import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
 
+      <SearchBar />
       <AddRecipeForm />
       <RecipeList />
       <FavoritesList />
