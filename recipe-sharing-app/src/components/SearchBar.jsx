73c459ecedad00658171b6b1267ac078f28f8d31
megamindjr <dkimathi605@gmail.com>
2026-01-25 15:16:00 +0300

    Create SearchBar.jsx

---
+++ recipe-sharing-app/src/components/SearchBar.jsx
@@ -0,0 +1,18 @@
+import { useRecipeStore } from '../store/recipeStore';
+
+const SearchBar = () => {
+  const searchTerm = useRecipeStore((state) => state.searchTerm);
+  const setSearchTerm = useRecipeStore((state) => state.setSearchTerm);
+
+  return (
+    <input
+      type="text"
+      placeholder="Search recipes..."
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      style={{ marginBottom: '20px', width: '100%' }}
+    />
+  );
+};
+
+export default SearchBar;
