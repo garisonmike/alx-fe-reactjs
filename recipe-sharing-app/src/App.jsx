cea0292155e4b365b45166f0b53d3d9ceb57a663
megamindjr <dkimathi605@gmail.com>
2026-01-25 15:07:37 +0300

    Create App.jsx

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,19 @@
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+
+      <AddRecipeForm />
+      <RecipeList />
+      <FavoritesList />
+      <RecommendationsList />
+    </div>
+  );
+}
+
+export default App;
