5d4b30c0fd186ee0bafec6c6686d4bdf5e57f965
megamindjr <dkimathi605@gmail.com>
2026-02-19 16:19:20 +0300

    Create App.jsx

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from "react";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif" }}>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
